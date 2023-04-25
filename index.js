ee9c8c9a263b26690f37513e8e8d2e63a4b1d239
badermah <bmahdawi1@student.rccd.edu>
2023-04-24 21:06:11 -0700

    added shaped.js classes for shapes, and shapes.test.js and installed jest

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // Define shapes
-const shapes = {
-  circle: '<circle cx="150" cy="100" r="80" fill="${shapeColor}"/>',
-  triangle: '<polygon points="150,20 80,180 220,180" fill="${shapeColor}"/>',
-  square: '<rect x="50" y="50" width="200" height="100" fill="${shapeColor}"/>',
-};
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
 // Prompt user for input
 function init() {
@@ -44,16 +40,27 @@ function init() {
       const { text, textColor, shape, shapeColor } = answers;
 
       // Generate the SVG string using shape and colors
-      const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-      <rect width="100%" height="100%" fill="none"/>
-      ${shapes[shape].replace("${shapeColor}", shapeColor)}
-      <text x="150" y="100" text-anchor="middle" dy=".3em" font-size="48" fill="${textColor}">
-        ${text}
-      </text>
-    </svg>`;
+      let svgString;
+      switch (shape) {
+        case "circle":
+          const circle = new Circle(150, 100, 80);
+          svgString = circle.getSvgString(shapeColor, textColor, text); // Pass text variable as argument
+          break;
+        case "triangle":
+          const triangle = new Triangle(150, 50, 50, 250, 250, 250); // Update with your triangle coordinates
+          svgString = triangle.getSvgString(shapeColor, textColor, text); // Pass text variable as argument
+          break;
+        case "square":
+          const square = new Square(50, 50, 200); // Update with your square coordinates and size
+          svgString = square.getSvgString(shapeColor, textColor, text); // Pass text variable as argument
+          break;
+        default:
+          console.error("Invalid shape selection");
+          return;
+      }
 
       // Write the SVG string to a file
-      fs.writeFile("logo.svg", svg, (err) => {
+      fs.writeFile("logo.svg", svgString, (err) => {
         if (err) {
           console.error("Failed to create logo.svg:", err);
         } else {
